e92c282a6d4da7d3e23d9841cf7544858a87fe80
2010002 <k.arihara@jaist.ac.jp>
2021-08-09 08:47:57 +0900

    [update] Change file reading to read as appropriate

--- Program.cs
+++ Program.cs
@@ -12,13 +12,17 @@ namespace Arihara.GuideSmoke
   {
     static void Main(string[] args)
     {
-      int t = 510;
-      string folderPath = string.Format("./data/ObsSphere_32x32x32");
-      string outputPath = string.Format("./data/FTLE/ftle-{0}.txt", t);
-      FTLE ftle = new FTLE(folderPath, 1000, -1);
-      ftle.CalcFTLE(t);
-      ftle.WriteFTLE(outputPath, t);
-      // ftle.ShowFTLE(t);
+      string folderPath = string.Format("./data/ObsSphere_32x32x32_2");
+      FTLE ftle = new FTLE(folderPath, 1000, 1);
+      for (int t = 300; t < 601; t += 100)
+      {
+        Console.WriteLine("Start FTLE Calculation : t = {0}", t);
+        ftle.CalcFTLE(t);
+        // ftle.ShowFTLE(t);
+        string outputPath = string.Format("./data/FTLE/ftle-{0}.txt", t);
+        ftle.WriteFTLE(outputPath, t);
+        Console.WriteLine("End FTLE Calculation");
+      }
     }
   }
 }
